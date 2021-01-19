48fb95dcc9558d6f097f5d8ec49b02e2966c5a88
timbotimber <hitimstephens@gmail.com>
2021-01-19 16:36:08 +0000

    kevin's notes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 const game = new Game();
-
+console.log("this is the main file");
 function preload() {
-  game.preloadGame();
+	game.preloadGame();
 }
 function setup() {
-  createCanvas(600, 400);
-  game.setupGame();
+	createCanvas(600, 400);
+	game.setupGame();
 }
 function draw() {
-  clear();
-  game.drawGame();
+	clear();
+	game.drawGame();
 }
 
 // add the jump function here:
 function keyPressed() {
-  if (keyCode === 32) {
-    game.player.jump();
-  }
+	if (keyCode === 32) {
+		game.player.jump();
+	}
 }
